2a443b935bf0bb25113a097ab50b56d2c4c850af
Nawra Humaira <nawrahumaira5@gmail.com>
2026-09-17 22:42:44 +0800

    A.1 If-sederhana

---
+++ Praktikum-3/src/ifDemo.java
@@ -0,0 +1,11 @@
+public class ifDemo {
+    public static void main(String[] args) {
+        int nilai = 80;
+
+        if (nilai >= 75) {
+            System.out.println("Anda LULUS");
+
+
+        }
+    }
+}
