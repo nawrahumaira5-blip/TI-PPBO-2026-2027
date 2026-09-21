ceef7ce392bd7f8ec1a16575c1736bab82a4d87c
Nawra Humaira <nawrahumaira5@gmail.com>
2026-09-21 18:18:55 +0800

    A.3. KOMENTAR KODE

--- Praktikum-2/src/Helloworld.java
+++ Praktikum-2/src/Helloworld.java
@@ -6,5 +6,12 @@ public class Helloworld {
         System.out.print("Budi");
         System.out.println();
         System.out.println("Ini baris baru");
+
+        // Program menampilkan biodata sederhana.
+        System.out.println("Nama: Budi");
+
+        /* Baris di bawah ini
+           mencetak NIM mahasiswa. */
+        System.out.println("NIM: 12345");
     }
 }
