ece160b9dc6d5be9216f902487b243fc65f2bcbf
Nawra Humaira <nawrahumaira5@gmail.com>
2026-09-21 18:18:05 +0800

    A.2. STRUKTUR DASAR PROGRAM JAVA

--- Praktikum-2/src/Helloworld.java
+++ Praktikum-2/src/Helloworld.java
@@ -1,5 +1,10 @@
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, Nawra");
+
+        System.out.print("Nama: ");
+        System.out.print("Budi");
+        System.out.println();
+        System.out.println("Ini baris baru");
     }
 }
